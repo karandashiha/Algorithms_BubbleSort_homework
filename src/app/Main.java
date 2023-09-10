70848eae6b5cbae33016db2d42c88fa750b9cba0
Katy Reshetnyk <kreshetnyk@gmail.com>
2023-09-10 18:04:02 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+//Bubble sort
+public class Main {
+    public static void main(String[] args) {
+        double[] price = {9.99, 15.50, 7.25, 1.99, 18.97, 2.5, 10.99};
+
+        //Сортування цін в зростаючому порядку
+        for (int i = 0; i < price.length; i++) {
+            for (int j = 1; j < (price.length - i); j++) {
+                if (price[j - 1] > price[j]) {
+                    double temp = price[j - 1];
+                    price[j - 1] = price[j];
+                    price[j] = temp;
+                }
+            }
+        }
+        System.out.println("The price of a product:");
+        for (double price_sort : price)
+            System.out.print(price_sort + " ");
+
+        //Розрахунок середньої ціни за весь період.
+        double sum = 0;
+        for (double num : price)
+            sum += num;
+        double difference = sum/price.length;
+            System.out.printf("%n%nThe average price of the product:%n%.2f",difference);
+
+    }
+}
+
